dc351fbc184561dba8f2f482efb4d731f30e19a9
Mandasujeevana <sujeevanamanda@gmail.com>
2026-04-04 15:36:09 +0530

    fixed port for render

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,9 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Server is running");
 });
-app.listen(3000,()=>{
-    console.log("Server running on http://localhost:3000");
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log('Server running on port ${PORT}');
 });
 app.post("/add-student", async (req, res) => {
 
