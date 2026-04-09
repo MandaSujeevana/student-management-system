16bf7d96e7698ed7d2c300c359dd9b82a51709fa
Mandasujeevana <sujeevanamanda@gmail.com>
2026-04-09 18:09:22 +0530

    fixed console log and port

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.get("/",(req,res)=>{
 });
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`gi);
 });
 app.post("/add-student", async (req, res) => {
 
